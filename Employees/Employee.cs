22ec23dae94e8d533478a498ab3776db09d8747b
Teresa Hawkins <74428141+deartc@users.noreply.github.com>
2022-09-17 15:14:41 -0400

    commit first

---
+++ Employees/Employee.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employees
+{
+    internal class Employee
+    {
+        private int empID;
+        private DateTime birthday;
+        private int salary;
+        public int MyProperty { get; set; }
+       	public void Login(string loginName, string password)	
+        {	    LoginName = loginName;
+            private static Employee (DateTime birthday) => this.birthday = birthday;
+
+        public DateTime birthday Update(int salary, DateTime birthday)
+ {
+            Salary = salary;
+            Birthday =  birthday;
+            return "\"Welcome to the company, "employee"! Your salary is "salary" and your birthday is "birthday".\"";
+            }
+
+
+        }
+
+
+    }
+}
