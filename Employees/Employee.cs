6b3bb620f306c43c2d83b67bc4d5d69d2a4b7ab2
Teresa Hawkins <74428141+deartc@users.noreply.github.com>
2022-09-17 16:09:08 -0400

    commit

--- Employees/Employee.cs
+++ Employees/Employee.cs
@@ -6,24 +6,15 @@ using System.Threading.Tasks;
 
 namespace Employees
 {
-    internal class Employee
+    internal partial class Employee
     {
         private int empID;
         private DateTime birthday;
         private int salary;
         public int MyProperty { get; set; }
-       	public void Login(string loginName, string password)	
-        {	    LoginName = loginName;
-            private static Employee (DateTime birthday) => this.birthday = birthday;
-
-        public DateTime birthday Update(int salary, DateTime birthday)
- {
-            Salary = salary;
-            Birthday =  birthday;
-            return "\"Welcome to the company, "employee"! Your salary is "salary" and your birthday is "birthday".\"";
-            }
-
-
+        public void Login(string loginName, string password)
+        {
+            ;
         }
 
 
---
+++ Employees/Program1.cs
@@ -0,0 +1,54 @@
+﻿namespace Employees
+{
+    internal partial class Employee
+    {
+        class Program
+        {
+            static void Main(string[] args)
+            {
+
+
+                DateTime myDate = new DateTime(2015, 12, 25, 10, 30, 45);//the date
+                int year = myDate.Year; // 2015  
+                int month = myDate.Month; //12  
+                int day = myDate.Day; // 25  
+                int hour = myDate.Hour; // 10  
+                int minute = myDate.Minute; // 30  
+                int second = myDate.Second; // 45  
+                int weekDay = (int)myDate.DayOfWeek; 
+
+
+                TGCEmployeeArray empArray = new TGCEmployeeArray();  //Creating an empty list of employees.
+
+
+                int x;
+                do
+                {
+
+                    Console.WriteLine("1 : : opitins " + "\n2 : : employee" + "\n3 : : exit");//display user
+
+                    x = int.Parse(Console.ReadLine());
+
+
+                    if (x == 1)
+                    {
+
+
+
+                    // Console.WriteLine("Enter  date in DD/MM/YYYY format");  //and date.
+                    // DateTime aDay = DateTime.Parse(Console.ReadLine());//enter date today
+                    // Console.WriteLine("Welcome to the company : ");private static Employee (DateTime birthday) => this.birthday = birthday;
+
+        public DateTime birthday Update(int salary, DateTime birthday)
+ {
+            Salary = salary;
+            Birthday =  birthday;
+            return "\"Welcome to the company, "employee"! Your salary is "salary" and your birthday is "birthday".\"";
+            }
+
+
+        }
+
+
+    }
+}
